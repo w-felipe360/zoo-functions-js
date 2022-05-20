00a9513d1516ee122a24b313f6c0bf3f6fb66ec6
Willian Felipe <w.felipebraz@gmail.com>
2022-05-20 19:00:28 -0300

    requisito 1 e 2 feitos so vamo

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  if (ids.length === 0) {
+    return ids;
+  }
+  return species.filter((element) => ids.some((id) => id === element.id));
 }
-
 module.exports = getSpeciesByIds;

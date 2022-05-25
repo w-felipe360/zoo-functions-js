290ea63a93be79559d0afd7d21f2fac12f29b34f
Willian Felipe <w.felipebraz@gmail.com>
2022-05-25 17:40:19 -0300

    FEITO QUESTÃO 4 COM LINT

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,25 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+
+  const empregadosFilt = employees.filter((empregados) =>
+    empregados.managers.find((elemento) => elemento === id));
+  return empregadosFilt.length > 0;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  try {
+    const empregadinhos = employees.filter((element) =>
+      element.managers.includes(managerId));
+    return empregadinhos.map(
+      (element) => `${element.firstName} ${element.lastName}`,
+    );
+  } catch (error) {
+    return error.message;
+  }
 }
-
 module.exports = { isManager, getRelatedEmployees };

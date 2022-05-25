9154d6bce28b207b771c645b8b7f5a04cfb23ddc
Willian Felipe <w.felipebraz@gmail.com>
2022-05-25 19:12:17 -0300

    feito requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
-}
+  if (animal === undefined) {
+    const objFinal = {};
+    species.forEach((element) => {
+      objFinal[element.name] = element.residents.length;
+    });
+    return objFinal;
+  }
 
+  const animaisMagicos = species.find(
+    (element) => element.name === animal.specie,
+  );
+  if (animal.sex !== undefined) {
+    const sexo = animaisMagicos.residents.filter(
+      (elemento) => elemento.sex === animal.sex,
+    );
+    return sexo.length;
+  }
+  return animaisMagicos.residents.length;
+}
+console.log(countAnimals({ specie: 'giraffes', sex: 'male' }));
 module.exports = countAnimals;

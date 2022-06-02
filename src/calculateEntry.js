d6b80f1998941d19a2182d479ce8842d59c1a548
Willian Felipe <w.felipebraz@gmail.com>
2022-06-02 09:02:19 -0300

    feito questão 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const idades = entrants.map((element) => element.age);
+  const menorDe18 = idades.filter((element2) => element2 < 18).length;
+  const adultos = idades.filter(
+    (element2) => element2 >= 18 && element2 < 50,
+  ).length;
+  const velhos = idades.filter((element2) => element2 >= 50).length;
+  const objeto = { child: menorDe18, adult: adultos, senior: velhos };
+  return objeto;
 }
-
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined) {
+    return 0;
+  }
+  if (Object.keys(entrants).length === 0) {
+    return 0;
+  }
+
+  return (
+    countEntrants(entrants).child * data.prices.child
+    + countEntrants(entrants).adult * data.prices.adult
+    + countEntrants(entrants).senior * data.prices.senior
+  );
 }
 
 module.exports = { calculateEntry, countEntrants };

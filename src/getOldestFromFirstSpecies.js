bbff4fb6a203621097a4d07e2721540710b7a3ca
Willian Felipe <w.felipebraz@gmail.com>
2022-06-02 21:08:28 -0300

    10 feita com lint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,5 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {}
-
+function getOldestFromFirstSpecies(id) {
+  const empregado = data.employees.find((element) => element.id === id).responsibleFor;
+  const animais = data.species.find((elemento) => elemento.id === empregado[0]);
+  const reduzidos = animais.residents.reduce((acc, cur) => {
+    if (acc > cur.age) return acc;
+    return cur.age;
+  }, 0);
+  const animalrray = data.species.filter((elemento) => elemento.id === empregado[0]);
+  const final = animalrray.map((element) => element.residents);
+  const legal = final[0].find((element) => element.age === reduzidos);
+  return [`${legal.name}`, `${legal.sex}`, legal.age];
+}
 module.exports = getOldestFromFirstSpecies;
